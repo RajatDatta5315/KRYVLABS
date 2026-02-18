e1d08f44e8c0022201d8c9db148591a957e59b0e
RajatDatta5315 <rajatdatta90000@gmail.com>
2026-02-18 14:14:56 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,15 +13,19 @@ export default {
       },
       colors: {
         'kryv-cyan': 'hsl(188, 100%, 50%)',
-        'kryv-bg-dark': 'hsl(240, 10%, 4%)',
-        'kryv-panel-dark': 'hsl(240, 6%, 8%)',
-        'kryv-border-dark': 'hsl(240, 5%, 15%)',
-        'kryv-text-dark-primary': 'hsl(0, 0%, 95%)',
-        'kryv-text-dark-secondary': 'hsl(0, 0%, 60%)',
+        'kryv-bg-dark': 'hsl(240, 10%, 3.9%)',
+        'kryv-panel-dark': 'hsl(240, 6%, 10%)',
+        'kryv-border': 'hsl(240, 5%, 18%)',
+        'kryv-text-primary': 'hsl(0, 0%, 98%)',
+        'kryv-text-secondary': 'hsl(240, 5%, 65%)',
+      },
+       animation: {
+        shimmer: 'shimmer 2s linear infinite'
       },
       keyframes: {
         shimmer: {
-          '100%': { transform: 'translateX(100%)' },
+          from: { backgroundPosition: '0 0' },
+          to: { backgroundPosition: '-200% 0' },
         },
       },
     },
